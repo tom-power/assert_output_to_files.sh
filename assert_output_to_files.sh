b6cd34109a0f327cd63d6c22da8764af0b294b59
Tom Power <tpower21@gmail.com>
2020-04-28 18:57:43 +0100

    and the main file

---
+++ assert_output_to_files.sh
@@ -0,0 +1,6 @@
+function assert_output_to_files() {
+  current="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+  read -d '' input
+  export input
+  eval "$(source ${current}/src/run.sh)"
+}#!/bin/sh
